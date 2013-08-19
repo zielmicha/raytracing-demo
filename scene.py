be29eb45fb69d90fa55244f1fa40530811e05204
Michał Zieliński <michal@zielinscy.org.pl>
2013-08-19 11:35:48 +0200

    fix light

--- scene.py
+++ scene.py
@@ -5,30 +5,40 @@ CENTER = Vector3(0.5, 0.5, 0)
 
 class Scene(object):
     def __init__(self):
+        #lights.ambient = Vector3(0.03, 0.03, 0.03)
+        self.camera = Vector3(0, 0, -3.)
+
         lights = LightSet()
-        lights.point.append(Vector3(2, 2, -3.))
+        lights.add_point(Vector3(10, 10, -3.), Vector3(0.05, 0.05, 0.05))
+        lights.add_point(self.camera, Vector3(0.1, 0.1, 0.1))
         self.objects = [Sphere(lights)]
 
     def get_pixel(self, x, y):
         v = Vector3()
         for obj in self.objects:
-            v += obj.draw(x, y)
+            v += obj.draw(self.camera, x, y)
+
+        v.x = min(max(v.x, 0), 1)
+        v.y = min(max(v.y, 0), 1)
+        v.z = min(max(v.z, 0), 1)
 
         return v
 
 class LightSet(object):
     def __init__(self):
         self.point = []
+        self.ambient = Vector3()
+
+    def add_point(self, src, color=Vector3(1, 1, 1)):
+        self.point.append((src, color))
 
 class Sphere(object):
     def __init__(self, lightset):
-        self.a = Vector3(0, 0, -3.)
         self.c = Vector3(0, 0, 8.)
         self.r = 1
         self.lightset = lightset
 
-    def draw(self, x, y):
-        a = self.a
+    def draw(self, a, x, y):
         b = Vector3(x, y, 0)
         c = self.c
         r = self.r
@@ -43,10 +53,11 @@ class Sphere(object):
             t = (delta ** 0.5 - B) / (2 * A)
             x = (b - a) * t + a
             normal = (x - c).normalized()
-            intensity = 0
-            for light_src in self.lightset.point:
-                light = normal.dot(light_src)
-                intensity += light
-            return Vector3(1, 1, 1) * max(0, min(1, intensity))
+            color = Vector3()
+            for light_src, light_color in self.lightset.point:
+                light = max((x - light_src).dot(normal), 0)
+                color += light_color * light
+            color += self.lightset.ambient
+            return color
         else:
             return Vector3(0, 0, -delta / 10000)
